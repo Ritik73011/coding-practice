f0adbaf6dd4907ed14da2aec02977ba9ad2bd479
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-01-22 17:45:36 +0530

    some changes on product oage

--- combine-filter/src/Component/ProductPage/Product.jsx
+++ combine-filter/src/Component/ProductPage/Product.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import { useContext } from "react";
+import ProductCard from "../ProductCard/ProductCard";
+import ProductContext from "../../Context/Product";
+import Filter from "../Filter/Filter";
+import "./Product.css";
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { tempProd } = useContext(ProductContext);
 
-export default Product
+  return (
+    <div className="productPage">
+      <Filter />
+      <div>
+        {tempProd.length > 0 ? (
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3,1fr)",
+              gap: "16px",
+            }}
+          >
+            {tempProd.map((ele) => {
+              return <ProductCard key={ele.id} ele={ele} />;
+            })}
+          </div>
+        ) : (
+          <h3>No Product Found</h3>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
