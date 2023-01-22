abfa40c374095686fb1d54dc940b95b47fdeb0dd
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-01-22 20:46:48 +0530

    combine filter added

--- combine-filter/src/App.js
+++ combine-filter/src/App.js
@@ -1,12 +1,25 @@
 import ProductContext from './Context/Product.jsx'
 import Product from './Component/ProductPage/Product'
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [tempProd,setTempProd] = useState([]);
+  const [product,setProduct] = useState([]);
+ 
+  const fetchProduct = async()=>{
+    let responce = await fetch("https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json");
+    let data = await responce.json();
+    setTempProd(data);
+    setProduct(data);
+  }
+  useEffect(()=>{
+    fetchProduct();
+  },[])
   return (
     
     <div className="App">
-     <ProductContext.Provider value={{}}>
+     <ProductContext.Provider value={{tempProd,product,setTempProd,setProduct}}>
         <Product/>
      </ProductContext.Provider>
     </div>
--- combine-filter/src/Component/Filter/Filter.jsx
+++ combine-filter/src/Component/Filter/Filter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 import { useContext } from "react";
 import ProductContext from "../../Context/Product";
 import './Filter.css'
@@ -7,6 +8,7 @@ const Filter = () => {
   const {product,setTempProd} = useContext(ProductContext);
 
   const[color,setColor] = useState([]);
+  const[type,setType] = useState([]);
   const filterColor = ()=>{
     let filter = [];
     color.forEach((el)=>{
@@ -16,6 +18,27 @@ const Filter = () => {
       filter.push(...arr);
       setTempProd(filter);
     })
+    
+    if(filter.length>0){
+      type.forEach((el)=>{
+        const arr = filter.filter((ele)=>{
+          return ele.gender === el;
+        });
+        filter = [];
+        filter.push(...arr);
+        setTempProd(filter);
+      })
+    }else{
+      type.forEach((el)=>{
+        const arr = product.filter((ele)=>{
+          return ele.gender === el;
+        })
+        filter.push(...arr);
+        setTempProd(filter);
+      })
+    }
+
+    
     if(filter.length===0){
       setTempProd(product);
     }
@@ -28,8 +51,18 @@ const Filter = () => {
      }else{
       setColor(color.filter((ele)=> ele!==value));
      }
-     filterColor();
   }
+  const typeHandler = (e)=>{
+    const {value,checked} = e.target;
+    if(checked){
+     setType([...type,value])
+    }else{
+     setType(type.filter((ele)=> ele!==value));
+    }
+  }
+  useEffect(()=>{
+    filterColor();
+  },[color,type])
   return (
     <div className="filterSection">
         <p>Color</p>
@@ -37,16 +70,16 @@ const Filter = () => {
         <label htmlFor=""><input type="checkbox" onChange={handleColor} value='Blue' />Blue</label><br />
         <label htmlFor=""><input type="checkbox" onChange={handleColor} value='Green' />Green</label><br />
         <p>Gender</p>
-        <label htmlFor=""><input type="checkbox" value='Men' />Men</label><br />
-        <label htmlFor=""><input type="checkbox" value='Women' />Women</label><br />
-        <p>Price</p>
-        <label htmlFor=""><input type="checkbox" value='0' />0-Rs250</label><br />
-        <label htmlFor=""><input type="checkbox" value='250' />Rs251-450</label><br />
-        <label htmlFor=""><input type="checkbox" value='450' />Rs451</label><br />
-        <p>Type</p>
-        <label htmlFor=""><input type="checkbox" value='Polo' />Polo</label><br />
-        <label htmlFor=""><input type="checkbox" value='Hoodie' />Hoodie</label><br />
-        <label htmlFor=""><input type="checkbox" value='Basic' />Basic</label><br />
+        <label htmlFor=""><input type="checkbox" onChange={typeHandler} value='Men' />Men</label><br />
+        <label htmlFor=""><input type="checkbox" onChange={typeHandler} value='Women' />Women</label><br />
+        <p>Price</p> 
+        <label htmlFor=""><input type="checkbox"  value='0' />0-Rs250</label><br />
+        <label htmlFor=""><input type="checkbox"  value='250' />Rs251-450</label><br />
+        <label htmlFor=""><input type="checkbox"  value='450' />Rs451</label><br />
+        <p>Type</p> 
+        <label htmlFor=""><input type="checkbox"  value='Polo' />Polo</label><br />
+        <label htmlFor=""><input type="checkbox"  value='Hoodie' />Hoodie</label><br />
+        <label htmlFor=""><input type="checkbox"  value='Basic' />Basic</label><br />
     </div>
   );
 };
