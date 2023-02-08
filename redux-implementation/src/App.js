4dec91ceab26f004cdd74a3bff7757f01af0afe4
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-08 16:18:35 +0530

    routing added

--- redux-implementation/src/App.js
+++ redux-implementation/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Wallpapers from './Components/Wallpapers/Wallpapers';
+import {Routes,Route} from 'react-router'
+import SingleWallpaper from './Components/SingleWallpapers/SingleWallpaper';
 
 function App() {
   return (
     <div className="App">
-      <Wallpapers/>
+      <Routes>
+        <Route path='/' element={<Wallpapers/>}/>
+        <Route path='/:link' element={<SingleWallpaper/>}/>
+      </Routes>
     </div>
   );
 }
